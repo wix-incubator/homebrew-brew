768540acc2750f48a361ba88d74612d7828dd029
Leo Natan <lnatan@wix.com>
2017-12-03 14:48:18 +0200

    0.5.18

--- applesimutils.rb
+++ applesimutils.rb
@@ -1,8 +1,8 @@
 class Applesimutils < Formula
   desc 'Apple simulator utilities'
   homepage 'https://github.com/wix/AppleSimulatorUtils'
-  url 'https://raw.githubusercontent.com/wix/homebrew-brew/master/AppleSimulatorUtils-0.5.17.tar.gz'
-  sha256 'd3cee3a36969648d88a86d83c083fd4065ab61cc4b46aab316c9cc126b3f278a'
+  url 'https://raw.githubusercontent.com/wix/homebrew-brew/master/AppleSimulatorUtils-0.5.18.tar.gz'
+  sha256 'c7a192be3bbee138b91f76b2b9dedb73786d183712eff4f9a1d01a3454757a90'
   head 'https://github.com/wix/AppleSimulatorUtils.git'
 
   depends_on xcode: ['8.3', :build]
