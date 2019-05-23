2f43c71a9d9e8376d79beec7a1426fda595337d6
Leo Natan <leo.natan@outlook.com>
2019-05-23 16:41:49 +0300

    0.6.5

--- applesimutils.rb
+++ applesimutils.rb
@@ -2,14 +2,14 @@ class Applesimutils < Formula
   desc 'Apple simulator utilities'
   homepage 'https://github.com/wix/AppleSimulatorUtils'
   url 'https://github.com/wix/AppleSimulatorUtils/releases/download/0.6.5/AppleSimulatorUtils-0.6.5.tar.gz'
-  sha256 'f1a3cf7add301fc208ec5e03dca6ba45d0f9641eeb85dbe9bade2ea8b4c51fba'
+  sha256 '6095c4d7543f1ce499ac4e2e5498882880de4a7c2c61ead652b3bd8732c2cb1d'
   head 'https://github.com/wix/AppleSimulatorUtils.git'
 
   depends_on xcode: ['10.0', :build]
   
   bottle do
     root_url 'https://github.com/wix/AppleSimulatorUtils/releases/download/0.6.5'
-    sha256 'e061814d898b3e9b44b2f335118a91b5ca1d94eac38005525dbf89d4f131234e' => :mojave
+    sha256 '6c7e4fa6a8e25cb44eb1da806e405f90624854349a996e4351f3f15a967642f9' => :mojave
   end
 
   def install
