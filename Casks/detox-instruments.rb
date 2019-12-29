f9d91217737a07432e8b0bff5dc5752adcc1dfcd
Leo Natan <leo.natan@outlook.com>
2019-12-29 13:06:46 +0200

    Detox Instruments 1.12.11639

--- Casks/detox-instruments.rb
+++ Casks/detox-instruments.rb
@@ -2,7 +2,7 @@ cask 'detox-instruments' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/wix/DetoxInstruments/releases/download/1.12.11638/DetoxInstruments-v1.12.b11638.zip'
+  url 'https://github.com/wix/DetoxInstruments/releases/download/1.12.11639/DetoxInstruments-v1.12.b11639.zip'
   name 'Detox Instruments'
   homepage 'https://github.com/wix/DetoxInstruments'
 
