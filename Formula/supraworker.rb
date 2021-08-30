fbc901a11424c36fd670121052440c28179b409c
Valera <weldpua2008@users.noreply.github.com>
2021-08-30 15:20:29 +0300

    Update supraworker.rb

--- Formula/supraworker.rb
+++ Formula/supraworker.rb
@@ -1,7 +1,7 @@
 class Supraworker < Formula
   desc "Supraworker is generic worker which picks Jobs to be executed from your API"
   homepage ""
-  url "https://github.com/weldpua2008/supraworker/releases/download/v0.2.2/supraworker_darwin_amd64.zip"
+  url "https://github.com/weldpua2008/supraworker/releases/download/v0.4.4/supraworker_darwin_amd64.zip"
   license "Apache-2.0"
   version :latest
   sha256 :no_check
