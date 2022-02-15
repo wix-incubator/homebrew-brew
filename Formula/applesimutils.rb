fde283414aafc901b4488355935a4d465c761621
Asaf Korem <asaf.korem@gmail.com>
2022-02-15 12:38:12 +0200

    Fix SHA256.

--- Formula/applesimutils.rb
+++ Formula/applesimutils.rb
@@ -11,12 +11,12 @@ class Applesimutils < Formula
   bottle do
     root_url 'https://github.com/wix/AppleSimulatorUtils/releases/download/0.9.5'
 
-    sha256 arm64_big_sur: "a903f253d6789f39b8930bc365fe958a9c526d63a23ae33a4465893cf9353472"
-    sha256 catalina:      "a903f253d6789f39b8930bc365fe958a9c526d63a23ae33a4465893cf9353472"
-    sha256 mojave:        "a903f253d6789f39b8930bc365fe958a9c526d63a23ae33a4465893cf9353472"
-    sha256 high_sierra:   "a903f253d6789f39b8930bc365fe958a9c526d63a23ae33a4465893cf9353472"
-    sha256 sierra:        "a903f253d6789f39b8930bc365fe958a9c526d63a23ae33a4465893cf9353472"
-    sha256 big_sur:       "a903f253d6789f39b8930bc365fe958a9c526d63a23ae33a4465893cf9353472"
+    sha256 arm64_big_sur: "f597cc8f4c09bf130921087565762d39eb9e79436df3ce8f5b700e372f5258bb"
+    sha256 catalina:      "f597cc8f4c09bf130921087565762d39eb9e79436df3ce8f5b700e372f5258bb"
+    sha256 mojave:        "f597cc8f4c09bf130921087565762d39eb9e79436df3ce8f5b700e372f5258bb"
+    sha256 high_sierra:   "f597cc8f4c09bf130921087565762d39eb9e79436df3ce8f5b700e372f5258bb"
+    sha256 sierra:        "f597cc8f4c09bf130921087565762d39eb9e79436df3ce8f5b700e372f5258bb"
+    sha256 big_sur:       "f597cc8f4c09bf130921087565762d39eb9e79436df3ce8f5b700e372f5258bb"
   end
 
   depends_on xcode: ["8.0", :build]
