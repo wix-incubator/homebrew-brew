42574f64007ad9e6554f1c9384ab4013964e978a
Asaf Korem <asaf.korem@gmail.com>
2022-10-30 16:16:25 +0200

    Apple Simulator Utils 0.9.8

--- Formula/applesimutils.rb
+++ Formula/applesimutils.rb
@@ -4,12 +4,12 @@
 class Applesimutils < Formula
   desc "Apple simulator utilities"
   homepage "https://github.com/wix/AppleSimulatorUtils"
-  url 'https://github.com/wix/AppleSimulatorUtils/releases/download/0.9.7/AppleSimulatorUtils-0.9.7.tar.gz'
-  sha256 '66f03496e512a233f1c6b542f33a256adb8c1233a61bff5e2c4c490b8013f1bf'
+  url 'https://github.com/wix/AppleSimulatorUtils/releases/download/0.9.8/AppleSimulatorUtils-0.9.8.tar.gz'
+  sha256 '251524acb6372b7accfa443755b6803401e460192e95dac9883a5275bb0b859e'
   head "https://github.com/wix/AppleSimulatorUtils.git"
 
   bottle do
-    root_url 'https://github.com/wix/AppleSimulatorUtils/releases/download/0.9.7'
+    root_url 'https://github.com/wix/AppleSimulatorUtils/releases/download/0.9.8'
 
     sha256 arm64_big_sur: "c628358da0ff7617b326f2d7ac77f1527f93096418ab21e6ac7f77de34bdad4f"
     sha256 catalina:      "c628358da0ff7617b326f2d7ac77f1527f93096418ab21e6ac7f77de34bdad4f"
